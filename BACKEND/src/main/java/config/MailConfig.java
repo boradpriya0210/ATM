cfb58ee6852f3bc37209668ee06e15e35ff4c92b
boradpriya0210 <boradpriya123@gmail.com>
2026-05-12 13:15:52 +0530

    Switch to SSL Port 465 for reliability

--- BACKEND/src/main/java/config/MailConfig.java
+++ BACKEND/src/main/java/config/MailConfig.java
@@ -7,7 +7,7 @@ import javax.mail.Session;
 
 public class MailConfig {
     public static final String HOST = "smtp.gmail.com";
-    public static final String PORT = "587";
+    public static final String PORT = "465";
     public static final String FROM_EMAIL = "boradpriya0210@gmail.com";
     public static final String APP_PASSWORD = "zoxo xzuw qplt qwwz";
 
@@ -16,9 +16,11 @@ public class MailConfig {
         properties.put("mail.smtp.host", HOST);
         properties.put("mail.smtp.port", PORT);
         properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.socketFactory.port", PORT);
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
         return Session.getInstance(properties, new Authenticator() {
+
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(FROM_EMAIL, APP_PASSWORD);
